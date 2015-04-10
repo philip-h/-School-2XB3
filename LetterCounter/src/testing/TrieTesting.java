788683d603d72633d29f11bf05b90df60ab877b2
Theo Stone <theo-falcon@live.ca>
2015-04-09 21:12:35 -0400

    Added: JUnit Testing for Trie

---
+++ LetterCounter/src/testing/TrieTesting.java
@@ -0,0 +1,66 @@
+package testing;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import datastructs.Trie;
+
+public class TrieTesting {
+
+	Trie t;
+	char[] alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ".toCharArray();
+	
+	@Before
+	public void setUp()
+	{
+		t = new Trie();
+	}
+	
+	@Test
+	public void testEmpty() 
+	{
+		for(int i = 0; i < alphabet.length; i++)
+		{
+			assertTrue(!t.contains(Character.toString(alphabet[i])));
+		}
+	}
+	
+	@Test
+	public void testCountSingles()
+	{
+		// Add Characters 10 times.
+		for(int q = 1; q < 10; q++)
+		{
+			for(int i = 0; i < alphabet.length; i++)
+			{
+				t.put(Character.toString(alphabet[i]));
+			}
+			
+			for(int i = 0; i < alphabet.length; i++)
+			{
+				assertEquals(t.getCount(Character.toString(alphabet[i])), q);
+			}
+		}
+	}
+	
+	@Test
+	public void testCountDoubles()
+	{
+		//Add Double Characters 10 Times.
+		for(int q = 1; q < 10; q++)
+		{
+			for(int i = 0; i < alphabet.length; i++)
+			{
+				t.put(Character.toString(alphabet[i]) + "D");
+			}
+			
+			for(int i = 0; i < alphabet.length; i++)
+			{
+				assertEquals(t.getCount(Character.toString(alphabet[i]) + "D"), q);
+			}
+		}
+	}
+
+}
